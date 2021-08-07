ddca839af9441492a9fc7cbc0d2f3a49da3c6469
Zeko <41507889+ZekoTheFox@users.noreply.github.com>
2021-08-07 03:30:27 -0700

    Add `001_hello_world.py`.

---
+++ L1/001_hello_world.py
@@ -0,0 +1,28 @@
+# [Lesson Header Information]
+# Level: Beginner, Newcomer
+# Name: "Hello World!"
+# Language: Python 3.9.6
+# Author: ZekoTheFox, https://github.com/ZekoTheFox
+# [Lesson Content Information]
+#     In this (beginner) lesson file, you will learn to write a common of set of
+# letters to the shell, known as the Hello World program.
+# This program will reply to you in your opened shell the two words:
+# 
+#     Hello, World!
+# 
+# [Lesson Info End]
+
+# Hello World in Python
+print("Hello, World!")
+
+# Congratulations! You have just written your first ever Python program.
+
+# 
+# ! TECHNICAL INFORMATION
+#
+# ? | Function Breakdown                     |
+#   +--------------------+---+-----------+---+
+#   | print              | ( | string    | ) |
+#   +--------------------+---+-----------+---+
+#   | Function Reference |===| Arg. Type |===|
+# 
---
+++ README.md
@@ -0,0 +1,6 @@
+# ZekoTheFox's Python Tutorial
+Something to do while I'm in school thats also to help out with other students.
+
+### Files
+`L1`
+- `001_hello_world.py` - Hello world program in Python.
