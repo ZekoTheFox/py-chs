41ef5a17f4a17b532c9ef4a853a0095916258427
Zeko <41507889+ZekoTheFox@users.noreply.github.com>
2021-08-09 02:56:14 -0700

    Add `002_addition_and_subtraction.py`.

---
+++ L1/002_addition_and_subtraction.py
@@ -0,0 +1,57 @@
+# [Lesson Header Information]
+# Level: Beginner, Newcomer
+# Name: "Hello World!"
+# Language: Python 3.9.6
+# Author: ZekoTheFox, https://github.com/ZekoTheFox
+# Requirements: Python Shell
+# [Lesson Content Information]
+#     In this (beginner) lesson file, you will learn to add and subtract within
+# the specific syntax of the Python.
+# [Lesson Info End]
+
+# Ignore this line.
+print(
+    # Do not add the comma, it is unnecessary for the shell.
+    
+    # Simply stating the two numbers with the Plus symbol between them will add.
+    1 + 2,
+    # = 3
+
+    5 + 7 - 3,
+    # = 9
+
+    # Division
+    3 * 6 / 9,
+    # = 2(.0)
+
+    # P.E.M.D.A.S. exists for the most part, however slightly modified.
+    # The next three examples demonstrate that.
+
+    # Parentheses will always be evaluated first, no matter what.
+    5 + (2 * 5),
+    # = 15
+
+    # You can't do X(Y), as that would try to invoke a function. (later lesson)
+    # You can however, just simply add the multiplicate sign, aka *, instead.
+    5 * (2 + 5),
+    # = 35
+
+    # Parentheses can be embedded within itself.
+    ((4 + 8) * 3) + 6,
+    # = 42
+
+    # Modulus uses the percentage symbol (Shift + 5).
+    # In radically simplified terms, the % symbol gets the remainder of the #
+    #   by the given number.
+    48 % 12,
+    # = 0
+
+    27 % 7,
+    # = 6
+
+# End of closing parenthesis
+)
+
+# Assignment for this file:
+# - In what way is this similar to the way you write mathematical equations in
+#   the real world?
